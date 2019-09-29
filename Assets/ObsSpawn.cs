c5eafba02b8ff4c2d93a0ff9f4c9a1619bc314e7
billkapes <kapes15@yahoo.com>
2019-09-28 21:38:20 -0500

    misc 2

--- Assets/ObsSpawn.cs
+++ Assets/ObsSpawn.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class ObsSpawn : MonoBehaviour
 {
+    public GameObject[] Obs;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach (var ob in Obs)
+        {
+            ob.transform.Translate(new Vector3(Random.Range(-2, 2), Random.Range(-1, 1), 0));
+            if (Random.Range(0, 2) > 0)
+            {
+                ob.transform.Rotate(Vector3.forward, 90);
+
+            }
+        }   
     }
 
     // Update is called once per frame
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -68,52 +68,69 @@ public class PlayerMovement : MonoBehaviour
 
     private void HandleMovement(Vector2 vel)
     {
+        if (currentThrust == 0f && vel != Vector2.zero)
+            return;
+
         myTurn = false;
+
+        GetComponent<LineRenderer>().SetPosition(1, Vector3.zero);
+
+        myVelocity.Set(myVelocity.x + vel.x * currentThrust, myVelocity.y + vel.y * currentThrust);
+
+        SetJet(vel);
+
+        TweenSlider(vel);
+
+
+        if (Physics.Raycast(transform.position, myVelocity, out RaycastHit hit, myVelocity.magnitude))
+        {
+            print("Found an object - distance: " + hit.distance);
+            Vector3 newPos = hit.point;
+            newPos = Vector3.MoveTowards(newPos, transform.position, 0.5f);
+
+            //Vector3 newPos = transform.position.normalized - hit.point;
+            //Vector3 newPos = (hit.point - transform.position).normalized;
+
+            //Vector3 newPos = Vector3.MoveTowards(hit.point, transform.position, 0.5f);
+            newPos = new Vector3(Mathf.Round(newPos.x), Mathf.Round(newPos.y), 0f);
+
+            transform.DOMove(newPos, moveDuration, false).OnComplete(FinishMove);
+            myVelocity = Vector3.zero;
+
+        }
+        else
+            transform.DOMove(transform.position + new Vector3(myVelocity.x, myVelocity.y, 0f), moveDuration, false).OnComplete(FinishMove);
+
+        hair.transform.localScale = Vector3.one * myVelocity.magnitude / 2f;
+
+
+    }
+
+    private void SetJet(Vector2 vel)
+    {
         if (currentThrust >= 1 && vel != Vector2.zero)
         {
             jet.transform.localPosition = new Vector3(-vel.x, -vel.y, 0.1f);
             jet.transform.localScale = new Vector3(currentThrust, currentThrust, jet.transform.localScale.z);
             jet.SetActive(true);
         }
+    }
 
-        GetComponent<LineRenderer>().SetPosition(1, Vector3.zero);
-
-        
-        myVelocity.Set(myVelocity.x + vel.x * currentThrust, myVelocity.y + vel.y * currentThrust);
-
+    private void TweenSlider(Vector2 vel)
+    {
         if (vel == Vector2.zero)
         {
             currentThrust = Mathf.Clamp(currentThrust + 1, 1, 3);
-            //thrustSlider.value = currentThrust;
 
             DOTween.To(() => thrustSlider.value, x => thrustSlider.value = x, currentThrust, moveDuration);
-
         }
         else
         {
             currentThrust = 0;
-            //currentThrust = Mathf.Clamp(currentThrust - 1, 0, 3);
-            //thrustSlider.value = currentThrust;
 
             DOTween.To(() => thrustSlider.value, x => thrustSlider.value = x, currentThrust, moveDuration);
 
         }
-
-
-        RaycastHit hit;
-
-        if (Physics.Raycast(transform.position, myVelocity, out hit, myVelocity.magnitude))
-        {
-            print("Found an object - distance: " + hit.distance);
-            transform.DOMove((hit.point), moveDuration, false).OnComplete(FinishMove);
-            myVelocity = Vector3.zero;
-           
-        } else
-            transform.DOMove(transform.position + new Vector3(myVelocity.x, myVelocity.y, 0f), moveDuration, false).OnComplete(FinishMove);
-
-        hair.transform.localScale = Vector3.one * myVelocity.magnitude / 2f;
-
-
     }
 
     private void FinishMove()
